f78cac356eddab1ae6aa437f6bab188af1072995
Yoier <huaimingdeng@gmail.com>
2025-06-22 13:20:15 +0800

    fix ";"

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,6 @@
 # See /LICENSE for more information.
 # This script was created by yoier <https://github.com/yoier/r3s-firmware-build/blob/main/diy-part2.sh>
 nowDate=`TZ="UTC-8" date "+%Y-%m-%d %H:%M:%S"`
-if [[ $1 == 'pre' ]] then ver=快照版; else ver=稳定版; fi 
+if [[ $1 == 'pre' ]]; then ver=快照版; else ver=稳定版; fi 
 sed -i "/return table/i table.appendChild(E('tr', { 'class': 'tr' }, [E('td', { 'class': 'td left', 'width': '33%' }, ['仓库地址 | 构建时间 | 版本']),E('td', { 'class': 'td left' }, [E('a', { 'href': 'https://github.com/yoier/r3s-firmware-build', 'target': '_blank' }, 'Powered by yoier/r3s-firmware-build | $nowDate | $ver')])]));" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 chmod +x files/scripts/*.sh
