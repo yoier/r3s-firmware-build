dafd23f2d2281873321d5ec90750b1138149c954
Yoier <huaimingdeng@gmail.com>
2026-02-03 01:09:13 +0800

    update passwall2 url

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,8 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 # This script was created by yoier <https://github.com/yoier/r3s-firmware-build/blob/main/diy-part1.sh>
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git passpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git passpackages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 git clone https://github.com/hudra0/qosmate.git package/qosmate
 git clone https://github.com/hudra0/luci-app-qosmate.git package/luci-app-qosmate
