0118af57fe942a5fb27946050d8a9fde07ef0c22
Yoier <huaimingdeng@gmail.com>
2026-04-26 12:10:00 +0800

    Cover source

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,15 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 # This script was created by yoier <https://github.com/yoier/r3s-firmware-build/blob/main/diy-part1.sh>
-echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git passpackages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+sed -i 's|https://git.openwrt.org/feed/packages.git|https://github.com/openwrt/packages.git|g' feeds.conf.default
+sed -i 's|https://git.openwrt.org/project/luci.git|https://github.com/openwrt/luci.git|g' feeds.conf.default
+sed -i 's|https://git.openwrt.org/feed/routing.git|https://github.com/openwrt/routing.git|g' feeds.conf.default
+sed -i 's|https://git.openwrt.org/feed/telephony.git|https://github.com/openwrt/telephony.git|g' feeds.conf.default
+cat > feeds.tmp <<'EOF'
+src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
+src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main
+src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master
+cat feeds.conf.default >> feeds.tmp
+mv feeds.tmp feeds.conf.default
 git clone https://github.com/hudra0/qosmate.git package/qosmate
 git clone https://github.com/hudra0/luci-app-qosmate.git package/luci-app-qosmate
