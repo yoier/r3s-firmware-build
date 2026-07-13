d68b17498f1ac765b782ae81b3ee33d7e2b3dfaf
yoier <huaimingdeng@gmail.com>
2026-07-13 20:01:34 +0800

    ln daed geo

--- files/scripts/firstboot.sh
+++ files/scripts/firstboot.sh
@@ -1,3 +1,5 @@
 # Script to run on first boot after update.Similar to uci-defaults/, but can be retained
 # This script is powered by yoier
 sed -i '/passwall2\|passwall_packages\|kenzo\|openwrtdaede\|vmlinuxbtf/d' /etc/apk/repositories.d/distfeeds.list
+ln -s /usr/share/v2ray/geosite.dat /usr/share/daed/geosite.dat
+ln -s /usr/share/v2ray/geoip.dat /usr/share/daed/geoip.dat
